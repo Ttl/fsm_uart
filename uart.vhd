090aac2673530be1fb27a7e92ddf32a6b2ae7a9a
Henrik Forstén <henrik.forsten@gmail.com>
2013-03-17 20:26:11 +0200

    Fix tabs, allow holding tx_req high

--- uart.vhd
+++ uart.vhd
@@ -5,9 +5,9 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 entity uart is
 generic (
-	CLK_FREQ		: integer := 32;		-- Main frequency (MHz)
-	SER_FREQ		: integer := 115200;	-- Baud rate (bps)
-	PARITY_BIT	    : boolean := true		-- Parity bit enable/disable
+	CLK_FREQ	: integer := 32;		-- Main frequency (MHz)
+	SER_FREQ	: integer := 9600;		-- Baud rate (bps)
+	PARITY_BIT	: boolean := true		-- Parity bit enable/disable
 );
 port (
 	-- Control
@@ -37,25 +37,25 @@ architecture Behavioral of uart is
 	type state_rx is (idle,data,parity);
 
 	-- RX Signals
-	signal rx_fsm			:	state_rx;						-- Control of reception
-	signal rx_clk_en		:	std_logic;						-- Received clock enable
-	signal rx_rcv_init	    :	std_logic;						-- Start of reception
-	signal rx_par_bit		:	std_logic;						-- Calculated Parity bit
-	signal rx_data_deb	    :	std_logic;						-- Debounce RX data
-	signal rx_data_tmp	    :	std_logic_vector(6 downto 0);	-- Serial to parallel converter
-	signal rx_data_cnt	    :	std_logic_vector(2 downto 0);	-- Count received bits
+	signal rx_fsm		:	state_rx;							-- Control of reception
+	signal rx_clk_en	:	std_logic;							-- Received clock enable
+	signal rx_rcv_init	:	std_logic;							-- Start of reception
+	signal rx_par_bit	:	std_logic;							-- Calculated Parity bit
+	signal rx_data_deb	:	std_logic;							-- Debounce RX data
+	signal rx_data_tmp	:	std_logic_vector(6 downto 0);	-- Serial to parallel converter
+	signal rx_data_cnt	:	std_logic_vector(2 downto 0);	-- Count received bits
 
 	-- TX Signals
-	signal tx_fsm			:	state_tx;					    -- Control of transmission
-	signal tx_init			:	std_logic;					    -- Resets tx_clk when tx_req is asserted
-	signal tx_start		    :	std_logic;						-- TX start signal for TX FSM
-	signal tx_clk_en		:	std_logic;					    -- Transmited clock enable
-	signal tx_par_bit		:	std_logic;					    -- Calculated Parity bit
-	signal tx_data_tmp	    :	std_logic_vector(7 downto 0);	-- Parallel to serial converter
-	signal tx_data_cnt	    :	std_logic_vector(2 downto 0);	-- Count transmited bits
+	signal tx_fsm		:	state_tx;							-- Control of transmission
+	signal tx_init		:	std_logic;							-- Resets tx_clk when tx_req is asserted
+	signal tx_start		:	std_logic;							-- TX start signal for FSM
+	signal tx_clk_en	:	std_logic;							-- Transmited clock enable
+	signal tx_par_bit	:	std_logic;							-- Calculated Parity bit
+	signal tx_data_tmp	:	std_logic_vector(7 downto 0);	-- Parallel to serial converter
+	signal tx_data_cnt	:	std_logic_vector(2 downto 0);	-- Count transmited bits
 	
 
--- Return a counter value that minimizes the error between real and requested baud rate.
+-- Return a counter value that minimizes the error between real and wanted baud rate.
 function counts(clk_freq, ser_freq : integer)
 	return integer is
 variable tmp : integer := (clk_freq*1_000_000)/ser_freq;
@@ -70,29 +70,29 @@ end counts;
 begin
 
 	tx_start_detect:process(clk)
-		variable tx_req_old	:	std_logic;
+		variable tx_req_old, tx_init_old : std_logic;
 	begin
 		if clk'event and clk = '1' then
-			-- Rising edge detection
-			if tx_req_old = '0' and tx_req = '1' and tx_fsm = idle then
+			if tx_init_old = '0' and tx_req = '1' and tx_fsm = idle then
 				tx_init		<=	'1';
 			else
 				tx_init		<=	'0';
-				tx_start	<= tx_req_old;
+				tx_start    <= tx_req_old;
 			end if;
 			-- Default assignments
-			tx_req_old			:=	tx_req;
+			tx_req_old	:=	tx_req;
+            tx_init_old := tx_init;
 			-- Reset condition
 			if rst = RST_LVL then
-				tx_req_old		:=	'0';
-				tx_start		<= '0';
-				tx_init			<=	'0';
+				tx_req_old	:=	'0';
+				tx_start	<= '0';
+				tx_init		<=	'0';
 			end if;
 		end if;
 	end process;
-	
+
 	tx_clk_gen:process(clk)
-		variable counter	:	integer range 0 to conv_integer(counts(CLK_FREQ, SER_FREQ));
+		variable counter : integer range 0 to conv_integer(counts(CLK_FREQ, SER_FREQ));
 	begin
 		if clk'event and clk = '1' then
 			-- Normal Operation
@@ -112,20 +112,20 @@ begin
 	end process;
 
 	tx_proc:process(clk)
-		variable data_cnt	: std_logic_vector(2 downto 0);
+		variable data_cnt : std_logic_vector(2 downto 0);
 	begin
 		if clk'event and clk = '1' then
-			tx_end					<=	'0';
+			tx_end	<=	'0';
 			if tx_clk_en = '1' then
 				-- Default values
-				tx						<=	UART_IDLE;
+				tx  <=	UART_IDLE;
 				-- FSM description
 				case tx_fsm is
 					-- Wait to transfer data
 					when idle =>
 						-- Send Init Bit
 						if tx_start = '1' then
-							tx				<=	UART_START;
+							tx			<=	UART_START;
 							tx_data_tmp	<=	tx_data;
 							tx_fsm		<=	data;
 							tx_data_cnt	<=	(others=>'1');
@@ -174,36 +174,36 @@ begin
 	end process;
 
 	rx_debounceer:process(clk)
-		variable deb_buf	:	std_logic_vector(3 downto 0);
+		variable deb_buf : std_logic_vector(3 downto 0);
 	begin
 		if clk'event and clk = '1' then
 			-- Debounce logic
 			if deb_buf = "0000" then
-				rx_data_deb		<=	'0';
+				rx_data_deb <=  '0';
 			elsif deb_buf = "1111" then
-				rx_data_deb		<=	'1';
+				rx_data_deb <=	'1';
 			end if;
 			-- Data storage to debounce
-			deb_buf				:=	deb_buf(2 downto 0) & rx;
+			deb_buf :=  deb_buf(2 downto 0) & rx;
 		end if;
 	end process;
 
 	rx_start_detect:process(clk)
-		variable rx_data_old	:	std_logic;
+		variable rx_data_old : std_logic;
 	begin
 		if clk'event and clk = '1' then
 			-- Falling edge detection
 			if rx_data_old = '1' and rx_data_deb = '0' and rx_fsm = idle then
-				rx_rcv_init		<=	'1';
+				rx_rcv_init	<=	'1';
 			else
-				rx_rcv_init		<=	'0';
+				rx_rcv_init	<=	'0';
 			end if;
 			-- Default assignments
-			rx_data_old			:=	rx_data_deb;
+			rx_data_old	:=	rx_data_deb;
 			-- Reset condition
 			if rst = RST_LVL then
-				rx_data_old		:=	'0';
-				rx_rcv_init		<=	'0';
+				rx_data_old	:=	'0';
+				rx_rcv_init	<=	'0';
 			end if;
 		end if;
 	end process;
@@ -233,7 +233,7 @@ begin
 	begin
 		if clk'event and clk = '1' then
 			-- Default values
-			rx_ready		<=	'0';
+			rx_ready <=	'0';
 			-- Enable on UART rate
 			if rx_clk_en = '1' then
 				-- FSM description
@@ -241,15 +241,15 @@ begin
 					-- Wait to transfer data
 					when idle =>
 						if rx_data_deb = UART_START then
-							rx_fsm		<=	data;
+							rx_fsm      	<=	data;
 						end if;
-						rx_par_bit		<=	'0';
-						rx_data_cnt		<=	(others=>'0');
+						rx_par_bit      	<=	'0';
+						rx_data_cnt	        <=	(others=>'0');
 					-- Data receive
 					when data =>
 						-- Check data to generate parity
 						if PARITY_BIT then
-							rx_par_bit		<=	rx_par_bit xor rx;
+							rx_par_bit  	<=	rx_par_bit xor rx;
 						end if;
 
 						if rx_data_cnt = 7 then
